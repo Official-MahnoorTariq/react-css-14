4a08fafeafd8e71c8a85ef92b47579565893a371
Official-MahnoorTariq <real.mahnoor.tariq@gmail.com>
2026-04-01 01:56:12 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Structure from "./components/Structure";
+import Title from "./components/Title";
+
+export default function App(){
+  return(
+    <>
+<Title/>
+  <Structure/>
+ 
+    </>
+  );
+}
